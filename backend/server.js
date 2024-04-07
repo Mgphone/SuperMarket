d03ab4b0274de301d2cfdd3c2a0dcf18b8b7b7f9
Mgphone <51868037+Mgphone@users.noreply.github.com>
2024-04-07 05:06:52 +0100

    mongo connection using dockercompose

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const mongoose = require("mongoose");
+const connection = process.env.MONGO_URL;
+if (!connection) {
+  console.log("no connection");
+}
+mongoose.connect(connection).then(() => console.log("Database is connected"));
 app.get("/", (req, res) => {
-  res.send("test?");
+  res.send("another test please will you??" + connection);
 });
+
 app.listen(port, () => {
   console.log("Server is listeing on port" + port);
 });
