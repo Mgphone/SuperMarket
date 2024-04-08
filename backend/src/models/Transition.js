a5d5183d630527d004ae959b27b0b265e880bcc3
Mgphone <51868037+Mgphone@users.noreply.github.com>
2024-04-09 00:16:45 +0100

    add model of rate and transition

---
+++ backend/src/models/Transition.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const TransitionsSchema = Schema({
+  branch: { type: mongoose.Schema.Types.ObjectId, ref: "branches" },
+  seller: { type: mongoose.Schema.Types.ObjectId, ref: "Users" },
+  buyer_name: { type: String, required: true },
+  byper_identity: { type: String, required: true },
+  currency: { type: String, required: true },
+  amount: { type: Number, required: true },
+  exchange_rate: { type: Number, required: true },
+  total_amount_in_bhat: { type: Number, required: true },
+  timestamp: { type: Date, default: Date.now },
+});
+module.exports = mongoose.model("Transitions", TransitionsSchema);
