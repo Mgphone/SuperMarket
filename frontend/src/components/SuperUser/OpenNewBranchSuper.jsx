5275c21aa262ce10a1c89c76e06fb207971d37f5
Mgphone <51868037+Mgphone@users.noreply.github.com>
2024-05-04 17:57:42 +0100

    add formik for  opennewbranchsuper

--- frontend/src/components/SuperUser/OpenNewBranchSuper.jsx
+++ frontend/src/components/SuperUser/OpenNewBranchSuper.jsx
@@ -1,44 +1,64 @@
 import { useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 function OpenNewBranchSuper({ handleclose, headers }) {
   const [openBranchError, setOpenBranchErrror] = useState(false);
-  const searchbranch = async (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const branchname = formData.get("branchname");
-    const openingamount = formData.get("openingamount");
-    const currency = formData.getAll("currency");
-    let myData = {
-      branchname: branchname,
-      openingamount: openingamount,
-      currency: currency,
-    };
+  const headersWithContent = {
+    ...headers,
+    "Content-Type": "application/json",
+  };
 
-    const headersWithContent = {
-      ...headers,
-      "Content-Type": "application/json",
+  const validationSchema = Yup.object({
+    branchname: Yup.string().required("Please Fill out branchname field"),
+    openingamount: Yup.string().required("Please fill out openingamount field"),
+    currency: Yup.array()
+      .min(1, "Please select at lease one Option of currency")
+      .required("Please select at lease one Value of currency"),
+  });
+
+  const handleSubmit = async (values) => {
+    console.log(values);
+    const formData = {
+      branchname: values.branchname,
+      openingamount: values.openingamount,
+      // currency: [],
     };
+    console.log(formData);
+
     try {
       const url = "/api/branches/createbranch";
       const response = await fetch(url, {
         method: "POST",
         headers: headersWithContent,
-        body: JSON.stringify(myData),
+        body: JSON.stringify(formData),
       });
 
       if (!response.ok) {
         throw new Error("Failed to create new Branch");
       }
       const createBranchJSON = await response.json();
-      createBranchJSON;
+      // createBranchJSON;
       alert(createBranchJSON.message);
       handleclose();
     } catch (error) {
-      console.error;
+      console.error(error);
       setOpenBranchErrror(error);
     }
   };
 
+  const formik = useFormik({
+    initialValues: {
+      branchname: "",
+      openingamount: "",
+      currency: [],
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      handleSubmit(values);
+    },
+  });
+  // console.log(formik);
   return (
     <div className="creatnewbranch">
       <button onClick={handleclose}>X</button>
@@ -48,30 +68,108 @@ function OpenNewBranchSuper({ handleclose, headers }) {
           <h3 className="error">Error Happen When creating your Branch</h3>
         </>
       )}
-
-      <form onSubmit={searchbranch}>
+      <form onSubmit={(e) => formik.handleSubmit(e)}>
         <label htmlFor="BranchName">
           Branch Name
-          <input type="text" id="branchname" name="branchname" />
+          {/* <input type="text" id="branchname" name="branchname" /> */}
+          <input
+            type="text"
+            name="branchname"
+            placeholder="branchname"
+            value={formik.values.branchname}
+            onChange={formik.handleChange}
+          />
         </label>
+        {formik.touched.branchname && formik.errors.branchname && (
+          <div className="errors">{formik.errors.branchname}</div>
+        )}
         <label htmlFor="OpeningAmount">
           Opening Amount
-          <input type="number" id="OpeningAmount" name="openingamount" />
+          {/* <input type="number" id="OpeningAmount" name="openingamount" /> */}
+          <input
+            type="number"
+            name="openingamount"
+            placeholder="opening amount"
+            value={formik.values.openingamount}
+            onChange={formik.handleChange}
+          />
         </label>
-
+        {formik.touched.openingamount && formik.errors.openingamount && (
+          <div className="errors">{formik.errors.openingamount}</div>
+        )}
         <fieldset>
           <legend>Select Currencies:</legend>
 
-          <input type="checkbox" id="USD" name="currency" value="USD" />
+          <input
+            type="checkbox"
+            id="USD"
+            name="currency"
+            value="USD"
+            checked={formik.values.currency.includes("USD")}
+            onChange={(e) => {
+              const updatedCurrency = [...formik.values.currency];
+              if (e.target.checked) {
+                updatedCurrency.push("USD");
+              } else {
+                updatedCurrency.splice(updatedCurrency.indexOf("USD"), 1);
+              }
+              formik.setFieldValue("currency", updatedCurrency);
+            }}
+          />
           <label htmlFor="USD">USD</label>
 
-          <input type="checkbox" id="GBP" name="currency" value="GBP" />
+          <input
+            type="checkbox"
+            id="GBP"
+            name="currency"
+            value="GBP"
+            checked={formik.values.currency.includes("GBP")}
+            onChange={(e) => {
+              const updatedCurrency = [...formik.values.currency];
+              if (e.target.checked) {
+                updatedCurrency.push("GBP");
+              } else {
+                updatedCurrency.splice(updatedCurrency.indexOf("GBP"), 1);
+              }
+              formik.setFieldValue("currency", updatedCurrency);
+            }}
+          />
           <label htmlFor="GBP">GBP</label>
 
-          <input type="checkbox" id="YEN" name="currency" value="YEN" />
+          <input
+            type="checkbox"
+            id="YEN"
+            name="currency"
+            value="YEN"
+            checked={formik.values.currency.includes("YEN")}
+            onChange={(e) => {
+              const updatedCurrency = [...formik.values.currency];
+              if (e.target.checked) {
+                updatedCurrency.push("YEN");
+              } else {
+                updatedCurrency.splice(updatedCurrency.indexOf("YEN"), 1);
+              }
+              formik.setFieldValue("currency", updatedCurrency);
+            }}
+          />
           <label htmlFor="YEN">YEN</label>
 
-          <input type="checkbox" id="KYAT" name="currency" value="KYAT" />
+          <input
+            type="checkbox"
+            id="KYAT"
+            name="currency"
+            value="KYAT"
+            checked={formik.values.currency.includes("KYAT")}
+            onChange={(e) => {
+              const updatedCurrency = [...formik.values.currency];
+              if (e.target.checked) {
+                updatedCurrency.push("KYAT");
+              } else {
+                updatedCurrency.splice(updatedCurrency.indexOf("KYAT"), 1);
+              }
+              formik.setFieldValue("currency", updatedCurrency);
+            }}
+          />
           <label htmlFor="KYAT">KYAT</label>
 
           <input
@@ -79,10 +177,26 @@ function OpenNewBranchSuper({ handleclose, headers }) {
             id="SINGDOLLAR"
             name="currency"
             value="SINGDOLLAR"
+            checked={formik.values.currency.includes("SINGDOLLAR")}
+            onChange={(e) => {
+              const updatedCurrency = [...formik.values.currency];
+              if (e.target.checked) {
+                updatedCurrency.push("SINGDOLLAR");
+              } else {
+                updatedCurrency.splice(
+                  updatedCurrency.indexOf("SINGDOLLAR"),
+                  1
+                );
+              }
+              formik.setFieldValue("currency", updatedCurrency);
+            }}
           />
           <label htmlFor="SINGDOLLAR">SINGDOLLAR</label>
         </fieldset>
-        <button>Submit</button>
+        {formik.touched.currency && formik.errors.currency && (
+          <div className="errors">{formik.errors.currency}</div>
+        )}
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
