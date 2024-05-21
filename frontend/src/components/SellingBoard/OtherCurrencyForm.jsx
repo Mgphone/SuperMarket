96e6800e0e6834a39dc4e6a0256cce7cbbfe2037
Mgphone <51868037+Mgphone@users.noreply.github.com>
2024-05-21 10:47:39 +0100

    initial commit for formik user submit buying form

--- frontend/src/components/SellingBoard/OtherCurrencyForm.jsx
+++ frontend/src/components/SellingBoard/OtherCurrencyForm.jsx
@@ -1,10 +1,32 @@
-function OtherCurrencyForm({ buyingCurrency, rates }) {
-  const { GBP, YEN, KYAT, SINDOLLAR } = rates[0];
+import { useState } from "react";
 
+function OtherCurrencyForm({ buyingCurrency, rates }) {
+  const [amount, setAmount] = useState(0);
+  const { GBP, YEN, KYAT, SINDOLLAR } = rates[0];
+  const handleAmountChange = (e) => {
+    setAmount(e.target.value);
+  };
+  const calculateTotal = (rates, buyingCurrency) => {
+    return amount * rates[0][buyingCurrency];
+  };
   return (
-    <div>
-      <h3>{buyingCurrency}BUY</h3>
-      {rates[0][buyingCurrency]}
+    <div className="sellingform">
+      <h3>{buyingCurrency} BUY</h3>
+      {/* {rates[0][buyingCurrency]} */}
+      <form>
+        <label htmlFor="EnterAmount">Please Enter the Amount </label>
+        <input
+          type="number"
+          name="enteramount"
+          placeholder="enteramount"
+          value={amount}
+          onChange={handleAmountChange}
+        ></input>
+
+        {rates && buyingCurrency && (
+          <label>Total in Bhat: {calculateTotal(rates, buyingCurrency)}</label>
+        )}
+      </form>
     </div>
   );
 }
