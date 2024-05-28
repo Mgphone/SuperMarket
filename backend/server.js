b0f4258a2fd9174db9525ea8d9055fda600a4d14
Mgphone <51868037+Mgphone@users.noreply.github.com>
2024-05-28 18:01:38 +0100

    testing

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
-require("dotenv").config();
+// require("dotenv").config();
 const express = require("express");
+const serverless = require("serverless-http");
 const app = express();
 const port = 5000;
 const { connectToDatabase } = require("./src/config/database");
@@ -16,6 +17,9 @@ app.use((req, res, next) => {
   console.log(`404 not found request:${req.method} respond:${req.originalUrl}`);
   res.status(404).json({ message: "Your End Point Not Found" });
 });
-app.listen(port, () => {
-  console.log("Server is listeing on port" + port);
-});
+module.exports.handler = serverless(app);
+if (process.env.NODE_ENV !== "production") {
+  app.listen(port, () => {
+    console.log("Server is listeing on port" + port);
+  });
+}
