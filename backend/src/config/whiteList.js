23b4d6b86945395140c5ef90a1e2b0b0723c8334
Mgphone <51868037+Mgphone@users.noreply.github.com>
2024-05-30 01:32:49 +0100

    change gitignore to add svg

--- backend/src/config/whiteList.js
+++ backend/src/config/whiteList.js
@@ -1,5 +1,6 @@
 const whiteList = [
   "http://localhost:5173",
-  "https://super-market-be.vercel.app",
+  "https://super-market-frontend.vercel.app",
+  "https://bhatmoneyexchange.netlify.app",
 ];
 module.exports = whiteList;
