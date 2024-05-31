41f6e0f6c4cc5d14bbc1db60422a493d226b3b3d
Mgphone <51868037+Mgphone@users.noreply.github.com>
2024-05-31 18:49:23 +0100

    change vercel json

--- backend/src/config/corsWildCard.js
+++ backend/src/config/corsWildCard.js
@@ -3,5 +3,7 @@ const corsWildCard = cors({
   origin: "*",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"],
+  exposedHeaders: ["Authorization"],
 });
 module.exports = corsWildCard;
