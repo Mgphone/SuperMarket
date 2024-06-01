361cdc503bef857bca6cb8afb5d54c4b36cff4ad
Mgphone <51868037+Mgphone@users.noreply.github.com>
2024-06-01 21:25:13 +0100

    add forgot password page

--- frontend/src/components/UserAuthenticationFront/Forgotpassword.jsx
+++ frontend/src/components/UserAuthenticationFront/Forgotpassword.jsx
@@ -1,5 +1,20 @@
+import { Link } from "react-router-dom";
 function forgotpassword() {
-  return <div>forgotpassword</div>;
+  return (
+    <div className="notfound">
+      <h1>Forgot Passsword??</h1>
+      <p>
+        No problem! We can help you reset your password. Depending on your role,
+        please follow these instructions:
+      </p>
+      Branch Manager: Contact Area Manager. Seller Staff: Contact Branch
+      Manager.
+      <p>They will be able to guide you through the password reset process.</p>
+      <button>
+        <Link to="/">Home</Link>
+      </button>
+    </div>
+  );
 }
 
 export default forgotpassword;
